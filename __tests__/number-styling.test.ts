eef039a5615a80c4cbba34089c61cec8ea43fe91
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2020-11-01 16:56:03 -0800

    Added rouding and percentage formatting

--- __tests__/number-styling.test.ts
+++ __tests__/number-styling.test.ts
@@ -1,8 +1,10 @@
 import {
     formatNumber,
-    stringIsNumber,
+    isNumber,
     addThousandsSeparators,
     truncateDecimals,
+    toPercentage,
+    roundDecimals,
 } from '../number-styling'
 
 const invalidNumbers = [
@@ -22,7 +24,7 @@ const invalidNumbers = [
 ]
 
 describe('Detect if a string is a number', () => {
-    const validNumbers = [
+    const validNumbers: (number | string)[] = [
         '100',
         '1,000',
         '1,000.0',
@@ -36,13 +38,14 @@ describe('Detect if a string is a number', () => {
         '1.',
         '000100.',
         '.123',
+        1,
     ]
     test.each(validNumbers)('%s is a valid number', (string) => {
-        expect(stringIsNumber(string)).toBe(true)
+        expect(isNumber(string)).toBe(true)
     })
 
     test.each(invalidNumbers)('%s is invalid: %s', (string) => {
-        expect(stringIsNumber(string)).toBe(false)
+        expect(isNumber(string)).toBe(false)
     })
 })
 
@@ -54,13 +57,14 @@ describe('Formats a number with thousands separators', () => {
         }
     )
 
-    const numbers: [string, string, string][] = [
+    const numbers: [string | number, string, string][] = [
         ['1,000', '1,000', 'already formatted'],
         ['1000', '1,000', 'add thousands separators'],
         ['31415926535', '31,415,926,535', 'add thousands separators'],
         ['10,0000,0', '1,000,000', 'remove incorrect separators'],
         ['123.123123123', '123.123123123', 'remove incorrect separators'],
         ['123,123.123123123', '123,123.123123123', "don't affect decimals"],
+        [1000, '1,000', 'also works with numbers'],
     ]
 
     test.each(numbers)('"%s" should be "%s": %s', (number, result) => {
@@ -81,7 +85,7 @@ describe('truncates decimal', () => {
         expect(truncateDecimals(string, 2)).toBe(string)
     })
 
-    const numbers: [string, number, string][] = [
+    const numbers: [string | number, number, string][] = [
         ['3.14159', 0, '3'],
         ['3.14159', 1, '3.1'],
         ['3.14159', 2, '3.14'],
@@ -94,11 +98,50 @@ describe('truncates decimal', () => {
         ['3.141', 5, '3.141'],
         ['3.1415', 5, '3.1415'],
         ['3.', 5, '3.'],
+        [100.123, 1, '100.1'],
     ]
     test.each(numbers)(
         'decimals are truncated: "%s" truncated by "%s" results in "%s"',
         (number, digits, result) => {
             expect(truncateDecimals(number, digits)).toBe(result)
+        }
+    )
+})
+
+describe('rounds decimal', () => {
+    test.each(invalidNumbers)(
+        'base case: strings that aren\'t valid numbers an empty string: "%s"',
+        (string) => {
+            expect(roundDecimals(string, 2)).toBe('')
+        }
+    )
+
+    const preformattedNumbers = ['123,123', '314159']
+    test.each(preformattedNumbers)('numbers without decimals remain unchanged: "%s"', (string) => {
+        expect(roundDecimals(string, 2)).toBe(string)
+    })
+
+    const numbers: [string | number, number, string][] = [
+        ['3.14159', 0, '3'],
+        ['3.14159', 1, '3.1'],
+        ['3.14159', 2, '3.14'],
+        ['3.14159', 3, '3.142'],
+        ['3.14159', 4, '3.1416'],
+        ['3.14159', 5, '3.14159'],
+        ['3', 5, '3'],
+        ['3.1', 5, '3.1'],
+        ['3.14', 5, '3.14'],
+        ['3.141', 5, '3.141'],
+        ['3.1415', 5, '3.1415'],
+        ['3.', 5, '3.'],
+        [100.123, 1, '100.1'],
+        ['3.49', 0, '3'],
+        ['3.5', 0, '4'],
+    ]
+    test.each(numbers)(
+        'decimals are rounded: "%s" truncated by "%s" results in "%s"',
+        (number, digits, result) => {
+            expect(roundDecimals(number, digits)).toBe(result)
         }
     )
 })
@@ -111,7 +154,7 @@ describe('formats number completely', () => {
         }
     )
 
-    const numbers: [string, string, string][] = [
+    const numbers: [string | number, string, string][] = [
         ['100', '100', 'already formatted'],
         ['1,000', '1,000', 'already formatted'],
         ['1,000.0', '1,000.0', 'already formatted'],
@@ -127,8 +170,30 @@ describe('formats number completely', () => {
         ['1.', '1', 'no dangling periods'],
         ['0.', '0', 'no dangling periods'],
         ['211.', '211', 'no dangling periods'],
+        [314159.26535, '314,159.26535', 'also works with numbers'],
     ]
     test.each(numbers)('"%s" should be "%s": "%s"', (number, result) => {
         expect(formatNumber(number)).toBe(result)
     })
+})
+
+describe('turns numbers into percentages', () => {
+    test.each(invalidNumbers)(
+        'base case: strings that aren\'t valid numbers an empty string: "%s"',
+        (string) => {
+            expect(toPercentage(string)).toBe('')
+        }
+    )
+
+    const numbers: [string | number, number, string][] = [
+        ['0.314159', 0, '31%'],
+        ['0.271828', 1, '27.2%'],
+        [0.123, 1, '12.3%'],
+    ]
+    test.each(numbers)(
+        'turned to percentage: "%s" truncated by "%s" results in "%s"',
+        (number, decimals, result) => {
+            expect(toPercentage(number, decimals)).toBe(result)
+        }
+    )
 })
--- number-styling.ts
+++ number-styling.ts
@@ -1,12 +1,13 @@
-export const stringIsNumber = (number: string): boolean => {
+export const isNumber = (number: string | number): boolean => {
+    if (typeof number === 'number') return true
     if (number === '') return false
     if (isNaN(parseFloat(number))) return false
     return Boolean(number.match(/^[\d,]*\.?[\d]*$/u))
 }
 
-export const addThousandsSeparators = (string: string): string => {
-    if (!stringIsNumber(string)) return ''
-    let number = string
+export const addThousandsSeparators = (input: string | number): string => {
+    if (!isNumber(input)) return ''
+    let number = String(input)
     number = number.replace(/,/giu, '')
     const parts = number.split('.')
     // eslint-disable-next-line unicorn/no-unsafe-regex -- laziness
@@ -14,19 +15,39 @@ export const addThousandsSeparators = (string: string): string => {
     return parts.join('.')
 }
 
-export const truncateDecimals = (string: string, decimals: number): string => {
-    if (!stringIsNumber(string)) return ''
-    const parts = string.split('.')
-    if (!parts[1]) return string
+export const truncateDecimals = (input: string | number, decimals: number): string => {
+    if (!isNumber(input)) return ''
+    const parts = String(input).split('.')
+    if (!parts[1]) return String(input)
     parts[1] = parts[1].slice(0, Math.max(0, decimals))
     let number = parts.join('.')
     number = number.replace(/[.]+$/u, '')
     return number
 }
 
-export const formatNumber = (string: string): string => {
-    if (!stringIsNumber(string)) return ''
-    let number = string
+export const roundDecimals = (input: string | number, decimals: number): string => {
+    if (!isNumber(input)) return ''
+    const parts = String(input).split('.')
+    if (!parts[1]) return String(input)
+    const roundingDigit = parts[1].slice(decimals, decimals + 1)
+    parts[1] = parts[1].slice(0, Math.max(0, decimals))
+    if (parseInt(roundingDigit) >= 5) {
+        if (decimals === 0) {
+            const lastDigit = parseInt(parts[0].slice(-1))
+            parts[0] = parts[0].slice(0, -1) + (lastDigit + 1)
+        } else {
+            const lastDigit = parseInt(parts[1].slice(-1))
+            parts[1] = parts[1].slice(0, -1) + (lastDigit + 1)
+        }
+    }
+    let number = parts.join('.')
+    number = number.replace(/[.]+$/u, '')
+    return number
+}
+
+export const formatNumber = (input: string | number): string => {
+    if (!isNumber(input)) return ''
+    let number = String(input)
     number = number.replace(/,/giu, '')
     number = number.replace(/^0+/u, '')
     if (number === '') number = '0'
@@ -34,4 +55,13 @@ export const formatNumber = (string: string): string => {
     number = number.replace(/[.]+$/u, '')
     number = addThousandsSeparators(number)
     return number
+}
+
+export const toPercentage = (input: string | number, decimals?: number) => {
+    if (!isNumber(input)) return ''
+    let number = typeof input === 'string' ? parseFloat(input) : input
+    number *= 100
+    let percentage = formatNumber(number)
+    if (typeof decimals !== 'undefined') percentage = roundDecimals(percentage, decimals)
+    return `${percentage}%`
 }
