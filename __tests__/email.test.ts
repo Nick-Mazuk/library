cc00ccc67c568d5d8fe445d76d68c7a04c670514
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2020-12-29 21:55:11 -0800

    fixed jest tests

--- __tests__/email.test.ts
+++ __tests__/email.test.ts
@@ -3,6 +3,6 @@ import { createMailtoLink } from '../email'
 describe('creates mailto url', () => {
     const recipients = ['hello@example.com']
     test.each(recipients)('creates email for "%s"', (recipient) => {
-        expect(createMailtoLink({ to: recipient })).toBe(`mailto:${recipient}`)
+        expect(createMailtoLink({ to: recipient })).toBe(`mailto:${recipient}?`)
     })
 })
