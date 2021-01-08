87e16dac6845e56fbe97f9aac4578e5c5ced3d9c
Nick Mazuk <Nick-Mazuk@users.noreply.github.com>
2021-01-08 10:18:37 -0800

    Added another test to isObjectEmpty

--- __tests__/object.test.ts
+++ __tests__/object.test.ts
@@ -5,7 +5,11 @@ describe('detect if an object is empty', () => {
         expect(isObjectEmpty({})).toBeTruthy()
     })
 
-    const filledObjects = [{ hello: 'world' }, { key1: 'value 1', key2: 'value 2' }]
+    const filledObjects = [
+        { hello: 'world' },
+        { key1: 'value 1', key2: 'value 2' },
+        { emptyString: '' },
+    ]
 
     test.each(filledObjects)('filled object "%s" returns false', (input) => {
         expect(isObjectEmpty(input)).toBeFalsy()
